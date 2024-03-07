6636e0e619a119509cfc9c387bb746258f62ad80
TheWinczi <mwinczewski@tutanota.com>
2024-03-07 08:38:40 +0100

    Fix issue with one image's channel; Reduce unused settings of datasets

--- src/datasets/dataset_config.py
+++ src/datasets/dataset_config.py
@@ -130,51 +130,25 @@ dataset_config = {
     },
     'pathmnist': {
         'path': join(_BASE_DATA_PATH, 'pathmnist'),
-        'resize': None,
-        'pad': 4,
-        'crop': 32,
-        'flip': True,
-        'normalize': None
-    },
-    'organamnist': {
-        'path': join(_BASE_DATA_PATH, 'organamnist'),
-        'resize': None,
-        'pad': 4,
-        'crop': 32,
-        'flip': True,
-        'normalize': None
     },
     'bloodmnist': {
         'path': join(_BASE_DATA_PATH, 'bloodmnist'),
-        'resize': None,
-        'pad': 4,
-        'crop': 32,
-        'flip': True,
-        'normalize': None
+    },
+    'organamnist': {
+        'path': join(_BASE_DATA_PATH, 'organamnist'),
+        'extend_channel': 3,
     },
     'tissuemnist': {
         'path': join(_BASE_DATA_PATH, 'tissuemnist'),
-        'resize': None,
-        'pad': 4,
-        'crop': 32,
-        'flip': True,
-        'normalize': None
+        'extend_channel': 3,
     },
     'skin7': {
         'path': join(_BASE_DATA_PATH, 'skin7'),
         'resize': (224, 224),
-        'pad': 4,
-        'crop': 32,
-        'flip': True,
-        'normalize': None
     },
     'skin8': {
         'path': join(_BASE_DATA_PATH, 'skin8'),
         'resize': (224, 224),
-        'pad': 4,
-        'crop': 32,
-        'flip': True,
-        'normalize': None
     }
 }
 
