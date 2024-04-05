edb4bb02bad726d42f8ead44de5de030780744fb
TheWinczi <mwinczewski@tutanota.com>
2024-04-05 15:14:14 +0200

    Adds script for cifar100 hyperparams searching

---
+++ scripts/gridsearch/cifar10x10.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Main constant training variables 
+WEIGHT_DEACAY=0.0005
+MOMENTUM=0.9
+BATCH_SIZE=128
+NETWORK="resnet18"
+TAU=3
+MAX_EXPERTS=1
+LR_MIN=0.0001
+GMMS=1
+DATASET="cifar100"
+NEPOCHS=200
+NUM_WORKERS=4
+NUM_TASKS=10
+SEED=0
+
+# Searched hyperparamiters
+LRs=(0.1 0.05 0.01 0.005 0.001)
+ALPHAs=(0.5 0.9 0.99 0.999 1)
+
+for LR in ${LRs[@]}; do
+    for ALPHA in ${ALPHAs[@]}; do
+        EXP_NAME="$ALPHA_$LR"
+
+        echo "========== | ========== | ========== | ========== | =========="
+        python src/main_incremental.py --approach seed --gmms &GMMS --max-experts $MAX_EXPERTS --use-multivariate --nepochs $NEPOCHS --tau $TAU --batch-size $BATCH_SIZE --num-workers $NUM_WORKERS --datasets $DATASET --num-tasks $NUM_TASKS --lr $LR --weight-decay $WEIGHT_DEACAY --alpha $ALPHA --use-test-as-val --network $NETWORK --momentum $MOMENTUM --exp-name $EXP_NAME --seed $SEED
+        echo "weightDecay = $WEIGHT_DEACAY," \
+            "momentum = $MOMENTUM," \
+            "batchSize = $BATCH_SIZE," \
+            "network = $NETWORK," \
+            "tau = $TAU" \
+            "maxExperts = $MAX_EXPERTS," \
+            "lrMin = $LR_MIN," \
+            "gmms = $GMMS," \
+            "dataset = $DATASET," \
+            "lr = $LR," \
+            "alfa = $ALFA"
+    done
+done
